c74f5892d46ea7424cdbf314ba3c3fcde83fb177
unknown <hurina.irina@mail.ru>
2022-10-22 07:23:02 +0300

    task 2

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0 */
+
+int GetNumber (string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+
+int[] RandomArray(int length)
+{
+    int[] array = new int[length];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(1, 100);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");        
+    }
+}
+
+int GetSum (int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sum += array[i];
+        }               
+    }
+    return sum;
+}
+
+
+int length = GetNumber("Введите размерность массива");
+int[] arr = RandomArray (length);
+PrintArray (arr);
+int sum = GetSum(arr);
+Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях {sum}");
