4cf030bdf66416158e239f256db3ff2838048c98
unknown <hurina.irina@mail.ru>
2022-10-22 06:58:42 +0300

    task 1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет 
+количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2 */
+
+int GetNumber (string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+
+int[] RandomArray(int length)
+{
+    int[] array = new int[length];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(100, 1000);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");        
+    }
+}
+
+int GetResult(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count += 1;
+        }
+    }
+    return count;
+}
+
+
+int length = GetNumber("Введите размерность массива:");
+int[] arr = RandomArray(length);
+PrintArray(arr);
+int count = GetResult(arr);
+Console.WriteLine($"Четных значений {count}");
